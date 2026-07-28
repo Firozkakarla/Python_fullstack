d2bc24c61b7809e94d0b466d9a469b55399a0f34
Kakarla Firoz <firozkakarla@gmail.com>
2026-07-28 10:48:27 +0000

    js

---
+++ js/test.js
@@ -0,0 +1,19 @@
+console.log("Hello, World")
+let student_name = "Sriram"
+console.log("Student Name: " + student_name)
+let student_age = 21
+let course_name = "Python Fullstack"
+console.log("Student Age: " + student_age, "Course Name: " + course_name)
+let fee =10000
+let discount = 10
+let discounted_fee = fee - (fee * discount / 100)
+console.log("Discounted Fee: " + discounted_fee)
+let age = 18
+if (age >= 18) {
+    console.log("Eligible for Admission")
+} else {
+    console.log("Not Eligible for Admission")
+}
+for(let i = 1; i <= 5; i++) {
+    console.log("Iteration number: " + i)
+}
